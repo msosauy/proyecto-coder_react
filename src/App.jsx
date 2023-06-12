3bd649804d926526fcd59220619b8988b8dd3269
msosa <uy.msosa@gmail.com>
2023-06-11 22:01:23 -0300

    ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
-  return <NavBar />
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer greetings={"Bienvenidos!!"}/>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import "./ItemListContainer.css";
+
+function ItemListContainer({greetings})  {
+    return (
+        <main>
+            <h1>{greetings}</h1>
+        </main>
+    )
+}
+
+export default ItemListContainer;
