5ddfd0a1bc1125fb0b9f261f32924160f24c9a16
msosa <uy.msosa@gmail.com>
2023-07-05 03:06:33 -0300

    2da preentrega final

--- src/components/NavBar/CompanyLogo/CompanyLogo.jsx
+++ src/components/NavBar/CompanyLogo/CompanyLogo.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import img from "./assets/logo.png"; 
 import "./CompanyLogo.css"
 
 
 function CompanyLogo() {
-  return <img className="nav__logo" src={img} alt="logo" />
+  return <Link to={"/"}><img className="nav__logo" src={img} alt="logo" /></Link>
 }
 
 export default CompanyLogo;
